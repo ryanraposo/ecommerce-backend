73dd5b385f1101327ddf4e1e39ad19253c654c06
Ryan Raposo <raposo.ryan@gmail.com>
2022-09-03 15:30:13 -0400

    add product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
+// GET /api/products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    // include associated Category and Tag data
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+          model: Category,
+          attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // get one product
